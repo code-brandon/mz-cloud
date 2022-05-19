1fcbf45291f8e747807d6225a0c84899a121408d
code-brandon <2459754464@qq.com>
2022-05-19 15:47:25 +0800

    feat(all):微服务项目初始化

---
+++ mz-modules/mz-system/system-provider/src/main/java/com/mz/system/provider/SystemProviderApplication.java
@@ -0,0 +1,13 @@
+package com.mz.system.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemProviderApplication.class, args);
+    }
+
+}
