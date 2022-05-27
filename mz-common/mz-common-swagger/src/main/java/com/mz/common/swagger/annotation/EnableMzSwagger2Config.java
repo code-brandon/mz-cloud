2f64725ed0b96f27e369060706ecc3df79d94ccc
code-brandon <2459754464@qq.com>
2022-05-27 14:17:28 +0800

    feat(mz-common-swagger):添加自定义Swagger2Config自动配置注解

---
+++ mz-common/mz-common-swagger/src/main/java/com/mz/common/swagger/annotation/EnableMzSwagger2Config.java
@@ -0,0 +1,27 @@
+package com.mz.common.swagger.annotation;
+
+import com.mz.common.swagger.config.Swagger3Config;
+import com.mz.common.swagger.config.SwaggerProperties;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.lang.annotation.*;
+
+/**
+ * What -- 自定义Swagger2Config自动配置注解
+ * <br>
+ * Describe --
+ * <br>
+ *
+ * @author 小政同学    QQ:xiaozheng666888@qq.com
+ * @ClassName: EnableMzSwagger3Config
+ * @CreateTime 2022/5/24 16:26
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Import({SwaggerProperties.class,Swagger3Config.class})
+@EnableSwagger2
+public @interface EnableMzSwagger2Config {
+}
