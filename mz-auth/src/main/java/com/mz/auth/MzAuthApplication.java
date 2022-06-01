f6bb40af961cc1f609b0c5e5cbfa090b49964222
code-brandon <2459754464@qq.com>
2022-06-01 12:30:09 +0800

    feat(mz-auth):集成Nacos

--- mz-auth/src/main/java/com/mz/auth/MzAuthApplication.java
+++ mz-auth/src/main/java/com/mz/auth/MzAuthApplication.java
@@ -2,8 +2,10 @@ package com.mz.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
+@EnableDiscoveryClient
 @EnableAuthorizationServer
 @SpringBootApplication
 public class MzAuthApplication {
