60cf027793dbd4ae1501b923d9cf01fe01ecdf01
code-brandon <2459754464@qq.com>
2022-06-01 18:15:53 +0800

    feat(mz-common-core): 核心模块添加工具类
    1.BeanMapUtils.java Bean与Map互转
    2.Minio 添加Mino工具类

---
+++ mz-common/mz-common-core/src/main/java/com/mz/common/core/utils/BeanMapUtils.java
@@ -0,0 +1,43 @@
+package com.mz.common.core.utils;
+
+import org.springframework.cglib.beans.BeanMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * What -- Bean与Map互转
+ * <br>
+ * Describe --
+ * <br>
+ *
+ * @author 小政同学    QQ:xiaozheng666888@qq.com
+ * @ClassName: BeanMapUtils
+ * @CreateTime 2022/6/1 18:13
+ */
+public class BeanMapUtils {
+
+    /**
+     * 将对象属性转化为map结合
+     */
+    public static <T> Map<String, Object> beanToMap(T bean) {
+        Map<String, Object> map = new HashMap<>();
+        if (bean != null) {
+            BeanMap beanMap = BeanMap.create(bean);
+            for (Object key : beanMap.keySet()) {
+                map.put(key+"", beanMap.get(key));
+            }
+        }
+        return map;
+    }
+
+    /**
+     * 将map集合中的数据转化为指定对象的同名属性中
+     */
+    public static <T> T mapToBean(Map<String, Object> map,Class<T> clazz) throws Exception {
+        T bean = clazz.newInstance();
+        BeanMap beanMap = BeanMap.create(bean);
+        beanMap.putAll(map);
+        return bean;
+    }
+}
