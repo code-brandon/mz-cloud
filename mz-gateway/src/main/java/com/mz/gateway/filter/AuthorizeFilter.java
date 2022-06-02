7d86c406385a4301d6627690b75997c744b139e4
code-brandon <2459754464@qq.com>
2022-06-02 18:27:48 +0800

    fix(mz-gateway): 网关取消拦截，仅作 清洗请求头中mz_from 参数,和日志输出

--- mz-gateway/src/main/java/com/mz/gateway/filter/AuthorizeFilter.java
+++ mz-gateway/src/main/java/com/mz/gateway/filter/AuthorizeFilter.java
@@ -1,16 +1,15 @@
 package com.mz.gateway.filter;
 
 import com.alibaba.fastjson.JSON;
+import com.mz.common.core.constants.SecurityConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
-import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -24,25 +23,18 @@ public class AuthorizeFilter implements GlobalFilter {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
         URI uri = request.getURI();
-        log.info("网关拦截到请求，其URI为：{}",uri);
+        log.info("网关拦截到请求，其URI为：{}", uri);
         // 1.获取请求参数
-        MultiValueMap<String, String> params =request.getQueryParams();
-        log.info("网关拦截到请求，其params为：{}", JSON.toJSONString(params.toSingleValueMap()));
+        MultiValueMap<String, String> params = request.getQueryParams();
+        log.debug("网关拦截到请求，其params为：{}", JSON.toJSONString(params.toSingleValueMap()));
 
-        // 2.获取authorization参数
+        // 1.1. 清洗请求头中mz_from 参数
+        request = request.mutate().headers(httpHeaders -> httpHeaders.remove(SecurityConstants.MZ_FROM)).build();
+
+        // 2.获取Headers参数
         HttpHeaders headers = request.getHeaders();
-        String auth = headers.getFirst("authorization");
-        log.info("网关拦截到请求，authorization：{}",auth);
-
-        // 3.校验
-        if (!StringUtils.isEmpty(auth) || uri.getPath().contains("/api-docs")) {
-            // 放行
-            return chain.filter(exchange);
-        }
-        // 4.拦截
-        // 4.1.禁止访问，设置状态码
-        exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
-        // 4.2.结束处理
-        return exchange.getResponse().setComplete();
+        log.debug("网关拦截到请求，headers：{}", JSON.toJSONString(headers.toSingleValueMap()));
+        // 放行
+        return chain.filter(exchange.mutate().request(request.mutate().build()).build());
     }
 }
