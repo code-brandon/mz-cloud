529b2f18eaa085e4d31dc86d1e78ff0b45870780
code-brandon <2459754464@qq.com>
2022-06-02 18:17:43 +0800

    feat(mz-common-core): 核心模块添加 安全变量类

---
+++ mz-common/mz-common-core/src/main/java/com/mz/common/core/constants/SecurityConstants.java
@@ -0,0 +1,19 @@
+package com.mz.common.core.constants;
+
+/**
+ * What --
+ * <br>
+ * Describe --
+ * <br>
+ *
+ * @Package: com.mz.common.core.constants
+ * @ClassName: SecurityConstants
+ * @Author: 小政同学    QQ:xiaozheng666888@qq.com
+ * @CreateTime: 2022/6/1 21:34
+ */
+public interface SecurityConstants {
+    String MZ_ROLE = "ROLE_";
+    String MZ_FROM_IN = "FROM_MZ";
+    String MZ_FROM = "mz_from";
+    String MZ_CLOUD_AUTH = Constant.OAUTH_KEY + "auth:";
+}
