346795d758f5111b77b58c402304272b7de831e2
code-brandon <2459754464@qq.com>
2022-06-02 18:30:33 +0800

    feat(mz-system-provider): 添加测试Api忽略 接口

--- mz-modules/mz-system/mz-system-provider/src/main/java/com/mz/system/provider/controller/UserController.java
+++ mz-modules/mz-system/mz-system-provider/src/main/java/com/mz/system/provider/controller/UserController.java
@@ -2,12 +2,16 @@ package com.mz.system.provider.controller;
 
 
 
+import cn.hutool.core.date.DateUtil;
+import com.mz.common.security.annotation.Ignore;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /**
  * What -- 资源服务器测试 控制器
@@ -29,5 +33,12 @@ public class UserController {
     @GetMapping("/getuser")
     public Object getUser(Authentication authentication) {
         return authentication.getPrincipal();
+    }
+
+    @Ignore
+    @ApiOperation( value = "测试Api忽略",tags = {"不进行授权"})
+    @GetMapping("/getIsIgnore")
+    public String getIsIgnore() {
+        return DateUtil.formatDateTime(new Date());
     }
 }
