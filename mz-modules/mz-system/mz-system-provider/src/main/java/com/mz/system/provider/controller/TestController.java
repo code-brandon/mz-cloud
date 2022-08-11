0ac154ce362285bd55c068ccce355e49c17abc4b
code-brandon <2459754464@qq.com>
2022-08-11 11:44:18 +0800

    style(mz-system-provider): 代码整理

--- mz-modules/mz-system/mz-system-provider/src/main/java/com/mz/system/provider/controller/TestController.java
+++ mz-modules/mz-system/mz-system-provider/src/main/java/com/mz/system/provider/controller/TestController.java
@@ -2,14 +2,9 @@ package com.mz.system.provider.controller;
 
 
 import cn.hutool.core.date.DateUtil;
-import com.mz.common.core.entity.R;
 import com.mz.common.security.annotation.Ignore;
-import com.mz.common.security.entity.MzSysUserSecurity;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,14 +26,6 @@ import java.util.Date;
 @RequestMapping("/test")
 @Api("资源服务器测试")
 public class TestController {
-
-    @ApiOperation("获取用户名")
-    @GetMapping("/getuser")
-    @PreAuthorize("hasRole('admin')")
-    public R<MzSysUserSecurity> getUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return R.ok().data((MzSysUserSecurity) authentication.getPrincipal());
-    }
 
     @Ignore
     @ApiOperation( value = "测试Api忽略",tags = {"不进行授权"})
