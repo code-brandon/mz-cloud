a1d24cce29054921f42f2bc0f25e950045ead879
code-brandon <2459754464@qq.com>
2022-09-30 01:32:51 +0800

    feat(mz-common-core): 添加授权状态码枚举类型

--- mz-common/mz-common-core/src/main/java/com/mz/common/core/exception/MzCodeEnum.java
+++ mz-common/mz-common-core/src/main/java/com/mz/common/core/exception/MzCodeEnum.java
@@ -33,12 +33,14 @@ public enum MzCodeEnum {
      */
     VAILD_EXCEPTION(10001,"参数格式校验失败"),
     SMS_CODE_EXCEPTION(10002,"验证码获取频率太高，请稍后再试"),
+    OAUTH_EXCEPTION(15000,"OAuth异常!"),
     USER_EXIST_EXCEPTION(15001,"用户存在!"),
     PHONE_EXIST_EXCEPTION(15002,"手机号存在!"),
     LOGINACCT_PASSWORD_INVAILD_EXCEPTION(15003,"账号或密码错误!"),
-    OAUTH_AUTH_EXCEPTION(15004,"OAuth_Auth异常!"),
+    OAUTH_AUTH_EXCEPTION(15004,"身份验证失败!"),
     OAUTH_ACCESS_EXCEPTION(15006,"OAuth_Access异常!"),
-    OAUTH_EXCEPTION(15007,"OAuth异常!"),
+    OAUTH_TOKEN_EXCEPTION(15007,"Token无效或过期!"),
+    OAUTH_GRANTTYPE_EXCEPTION(15008,"不支持的授予类型!"),
     FEIGN_EXCEPTION(20000,"Feign远程调用异常！！");
 
     /**
