e99494f17e9f90adc61a545b0c1bae0e9b6f0d48
code-brandon <2459754464@qq.com>
2022-09-30 01:32:02 +0800

    fix(mz-common-security): 修改注释

--- mz-common/mz-common-security/src/main/java/com/mz/common/security/config/MzTokenStoreConfig.java
+++ mz-common/mz-common-security/src/main/java/com/mz/common/security/config/MzTokenStoreConfig.java
@@ -33,11 +33,11 @@ public class MzTokenStoreConfig {
         tokenStore.setPrefix(Constant.OAUTH_KEY);
         // 将存放在redis中的值序列化为JSON TODO 测试发现使用后会出现 User 无法转为 MzSysUserSecurity
         // TODO 解决User 无法转为 MzSysUserSecurity：需要将 MzUserDetailsSecurity 实现 UserDetails, Serializable 接口，而非继承User
-        // {@link https://github.com/moutainhigh/aiadver-parent/tree/ab7d3db5d01aaad215810601946931c8ed9b9183/aiadver-project/aiadver-boot-starter-parent/aiadver-boot-starter-oauth2/src/main/java/com/aiadver/boot/oauth2/token/redis}
+        // {@like <a href="https://github.com/moutainhigh/aiadver-parent/tree/ab7d3db5d01aaad215810601946931c8ed9b9183/aiadver-project/aiadver-boot-starter-parent/aiadver-boot-starter-oauth2/src/main/java/com/aiadver/boot/oauth2/token/redis">文章地址</a>}
         tokenStore.setSerializationStrategy(new MzJackson2JsonSerializationStrategy());
         /**
-         * {@link https://www.csdn.net/tags/MtjaEg3sMTAzNzktYmxvZwO0O0OO0O0O.html}
-         * {@link https://blog.csdn.net/lki_suidongdong/article/details/105945477/}
+         * {@link <a href = "https://www.csdn.net/tags/MtjaEg3sMTAzNzktYmxvZwO0O0OO0O0O.html"></a>}
+         * {@link <a href="https://blog.csdn.net/lki_suidongdong/article/details/105945477/"></a>}
          * TODO 如果反序列化时报错，那请参照错误信息，进行修改。
          */
         // tokenStore.setSerializationStrategy(new MzFastjsonRedisSerializationStrategy());
