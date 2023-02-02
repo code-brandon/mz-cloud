b48bb5c19ca1a483551c696a41edabd51eaa6920
code-brandon <2459754464@qq.com>
2023-02-02 23:13:35 +0800

    feat(mz-common-validated): 整合数据校验模块

---
+++ mz-common/mz-common-validated/src/main/java/com/mz/common/validated/groups/InsertField.java
@@ -0,0 +1,17 @@
+package com.mz.common.validated.groups;
+
+import javax.validation.groups.Default;
+
+/**
+ * What -- 插入字段校验
+ * <br>
+ * Describe --
+ * <br>
+ *
+ * @Package: com.mz.common.validated.group
+ * @ClassName: UpdateField
+ * @Author: 小政同学    QQ:xiaozheng666888@qq.com
+ * @CreateTime: 2022/12/21 19:04
+ */
+public interface InsertField extends Default {
+}
---
+++ mz-common/mz-common-validated/src/main/java/com/mz/common/validated/groups/Other3Field.java
@@ -0,0 +1,17 @@
+package com.mz.common.validated.groups;
+
+import javax.validation.groups.Default;
+
+/**
+ * What -- 其他字段校验3
+ * <br>
+ * Describe --
+ * <br>
+ *
+ * @Package: com.mz.common.validated.group
+ * @ClassName: UpdateField
+ * @Author: 小政同学    QQ:xiaozheng666888@qq.com
+ * @CreateTime: 2022/12/21 19:04
+ */
+public interface Other3Field extends Default {
+}
