6b03647ad9bb7ab90c699c85e3fdb8479d81edcb
code-brandon <2459754464@qq.com>
2023-02-23 13:46:34 +0800

    fix(mz-common-mybatis): 调整Mybatis-plus 分页最大条数

--- mz-common/mz-common-mybatis/src/main/java/com/mz/common/mybatis/config/MzMybatisAutoConfiguration.java
+++ mz-common/mz-common-mybatis/src/main/java/com/mz/common/mybatis/config/MzMybatisAutoConfiguration.java
@@ -56,7 +56,7 @@ public class MzMybatisAutoConfiguration implements WebMvcConfigurer {
 		// 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
 		paginationInterceptor.setOverflow(true);
 		// 设置最大单页限制数量，默认 500 条，-1 不受限制
-		paginationInterceptor.setMaxLimit(10L);
+		paginationInterceptor.setMaxLimit(200L);
 		// 开启 count 的 join 优化,只针对部分 left join
 		paginationInterceptor.setOptimizeJoin(true);
 		interceptor.addInnerInterceptor(paginationInterceptor);
