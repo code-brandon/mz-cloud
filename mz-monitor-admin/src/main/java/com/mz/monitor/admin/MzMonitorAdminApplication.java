4eb46ae79cc6f980b309c4808dbd63b18b29f473
code-brandon <2459754464@qq.com>
2023-07-28 15:29:22 +0800

    feat(mz-monitor-admin): 新增springboot-admin 对服务进行监控

---
+++ mz-monitor-admin/src/main/java/com/mz/monitor/admin/MzMonitorAdminApplication.java
@@ -0,0 +1,16 @@
+package com.mz.monitor.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@EnableAdminServer
+@SpringBootApplication
+public class MzMonitorAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MzMonitorAdminApplication.class, args);
+    }
+
+}
---
+++ mz-monitor-admin/src/test/java/com/mz/monitor/admin/MzMonitorAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mz.monitor.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MzMonitorAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
