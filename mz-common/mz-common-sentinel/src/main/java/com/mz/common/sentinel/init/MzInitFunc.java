add24247d37ea808e36847ee920f76c3472d86f8
code-brandon <2459754464@qq.com>
2023-07-28 08:35:16 +0800

    fix(mz-common-sentinel): 优化代码

--- mz-common/mz-common-sentinel/src/main/java/com/mz/common/sentinel/init/MzInitFunc.java
+++ mz-common/mz-common-sentinel/src/main/java/com/mz/common/sentinel/init/MzInitFunc.java
@@ -3,21 +3,18 @@ package com.mz.common.sentinel.init;
 import com.alibaba.cloud.sentinel.SentinelProperties;
 import com.alibaba.cloud.sentinel.datasource.config.DataSourcePropertiesConfiguration;
 import com.alibaba.cloud.sentinel.datasource.config.RedisDataSourceProperties;
-import com.alibaba.csp.sentinel.datasource.Converter;
+import com.alibaba.cloud.sentinel.datasource.converter.JsonConverter;
 import com.alibaba.csp.sentinel.datasource.ReadableDataSource;
 import com.alibaba.csp.sentinel.datasource.redis.RedisDataSource;
 import com.alibaba.csp.sentinel.datasource.redis.config.RedisConnectionConfig;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.TypeReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -35,11 +32,11 @@ public class MzInitFunc implements ApplicationRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(MzInitFunc.class);
 
-    @Autowired
+    @Resource
     private SentinelProperties sentinelProperties;
 
-    @Value("${spring.application.name}")
-    private String appName;
+    @Resource(name = "sentinel-json-flow-converter")
+    public JsonConverter jsonFlowConverter;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -48,21 +45,21 @@ public class MzInitFunc implements ApplicationRunner {
         RedisDataSourceProperties redisDataSourceProperties = dataSource.getRedis();
 
         // TODO 手动实现转换器，框架会出现  c.a.c.s.d.converter.SentinelConverter    : converter can not convert rules because source is empty 警告
-        Converter<String, List<FlowRule>> parser = source -> JSON.parseObject(source,new TypeReference<List<FlowRule>>() {});
+        // Converter<String, List<FlowRule>> parser = source -> JSON.parseObject(source,new TypeReference<List<FlowRule>>() {});
         RedisConnectionConfig config = RedisConnectionConfig.builder()
                 .withHost(redisDataSourceProperties.getHost())
                 .withPort(redisDataSourceProperties.getPort())
                 .withPassword(redisDataSourceProperties.getPassword())
                 .build();
         // 流控规则
-        ReadableDataSource<String, List<FlowRule>> redisDataSource = new RedisDataSource(
+        ReadableDataSource<String, List<FlowRule>> redisDataSource = new RedisDataSource<>(
                 config,
-                redisDataSourceProperties.getRuleKey()+appName,
-                redisDataSourceProperties.getChannel()+appName,
-                parser
+                redisDataSourceProperties.getRuleKey(),
+                redisDataSourceProperties.getChannel(),
+                jsonFlowConverter
         );
         FlowRuleManager.register2Property(redisDataSource.getProperty());
 
-        logger.info("{}：拉取sentinel规则成功",appName);
+        logger.info("{}：拉取sentinel规则成功",redisDataSourceProperties.getRuleKey());
     }
 }
